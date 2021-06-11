59e4075731cc797dc60712d3a091c6292da825bc
김성규 <castleq37@gmail.com>
2021-06-11 17:59:30 +0900

    ADD: 상품 전체페이지 기능구현

--- products/views.py
+++ products/views.py
@@ -6,16 +6,7 @@ from .models import Category,CategoryFood,Food,FoodImage
 class FoodlistView(View):
     def get(self, request):
 
-
-        #  = request.GET.get('category')
-        # print('-------------')
-        # print(products)
-        # print('-------------')
-
         foods = Food.objects.all()
-        # print(dir(foods[0].foodimage_set))
-        # img = FoodImage.objects.first()
-        
         result = []
 
         for food in foods:
@@ -32,9 +23,3 @@ class FoodlistView(View):
             }
             result.append(food_info)
         return JsonResponse({'result' : result}, status=200)
-
-
-# 1. 전체리스트 불러오기 
-# 2. 카테고리 별로 불러오기
-# 3. restful.apl방식을 이용해서 필터링하기
-# 4. 불러온 리스트를 별점 높은순, 후기 많은순으로 정렬하기
