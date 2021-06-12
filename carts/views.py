e54cd31e0814e122c948f9261cdaf63e65f47be2
pkc035 <pkc0305@gmail.com>
2021-06-12 16:15:54 +0900

    Add : Cart 장바구니 담기 기능

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+import json
+import jwt
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from .models         import Cart
+from users.models    import User
+from products.models import Food, RequiredOption, SelectOption
+from my_settings     import SECRET_KEY
+
+def decode_password(token):
+    decoded_token = jwt.decode(token,SECRET_KEY, algorithms='HS256')
+
+    return decoded_token['id']
+
+class CartView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        
+        #임시 id 객체 가져오기
+        users = User.objects.all()
+
+        for food in data:
+            cart = Cart(
+                # user        = decode_password(request.headers['token']),
+                user        = users[2],
+                option_type = food['option_type'],
+                option_id   = food['option_id'],
+                count       = food['count']
+            )
+            cart.save()
+
+        return JsonResponse({'message':'SUCCESS'}, status=201)
--- products/views.py
+++ products/views.py
@@ -10,6 +10,34 @@ class FoodView(View):
     def get(self, request, food_id):
         try:
             food             = Food.objects.get(id=food_id)
+            required_options = RequiredOption.objects.filter(food=food_id)
+            select_options   = SelectOption.objects.filter(food=food_id)
+            images           = FoodImage.objects.filter(food=food_id)
+            
+            if not images.exists():
+                image = []
+            else:
+                image = [image.image_url for image in images]
+                
+            if not select_options.exists():
+                required_option = []
+            else:
+                required_option = [{
+                        'id'      : required_option.id,
+                        'food_id' : food.id,
+                        'name'    : required_option.name,
+                        'price'   : required_option.price,
+                    }for required_option in required_options]
+            
+            if not select_options.exists():
+                select_option = []
+            else:
+                select_option = [{
+                        'id'      : select_option.id,
+                        'food_id' : food.id,
+                        'name'    : select_option.name,
+                        'price'   : select_option.price,
+                    }for select_option in select_options]
 
             result = {
                 'name'             : food.name,
@@ -21,17 +49,9 @@ class FoodView(View):
                 'detail_image'     : food.detail_image,
                 'create_at'        : food.create_at,
                 'update_at'        : food.update_at,
-                'image'            : [image.image_url for image in FoodImage.objects.filter(food=food_id)],
-                'required_food'    : [{
-                        'food_id' : food.id,
-                        'name'    : required_option.name,
-                        'price'   : required_option.price,
-                    }for required_option in RequiredOption.objects.filter(food=food_id)],
-                'select_food'      : [{
-                        'food_id' : food.id,
-                        'name'    : select_option.name,
-                        'price'   : select_option.price,
-                    }for select_option in SelectOption.objects.filter(food=food_id)]
+                'image'            : image,
+                'required_food'    : required_option,
+                'select_food'      : select_option
             }
 
             return JsonResponse({'message':result}, status=200)
