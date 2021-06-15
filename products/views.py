9db1b0c0647bcd7918da607f12d625507876dca5
김성규 <castleq37@gmail.com>
2021-06-15 16:43:02 +0900

    [밀켓마캣] ADD: 별점순 리뷰많은순 정렬기능 추가

--- products/views.py
+++ products/views.py
@@ -1,37 +1,35 @@
-from django.http  import JsonResponse
-from django.views import View
+from django.http      import JsonResponse
+from django.views     import View
+from django.db.models import Q
 
 from .models import Category,CategoryFood,Food,FoodImage
+
 class FoodlistView(View):
     def get(self, request):
-        category_filter = request.GET.get('category')
+        category_id = request.GET.get('categoryId')
+        sort        = request.GET.get('sort','id')
 
-        if category_filter == None:
-            categoryfoods = CategoryFood.objects.all()
-        else:
-            category = Category.objects.get(id=category_filter)
-            categoryfoods = category.categoryfood_set.all() # 카테고리에 묶여있는 중간테이블 전부를 표현
+        q = Q()
 
+        if category_id:
+            q = Q(categories__id=category_id)
+
+        foods = Food.objects.filter(q).order_by(sort)
+        
         result = []
-        for food in categoryfoods:
+        for food in foods:
             
             food_info = {
-                'id'               : food.food.id, # 앞에 푸드는 for문의 푸드, 뒤는 테이블 컬럼명 푸드(정참조)
-                'img_url'          : food.food.foodimage_set.first().image_url,
-                'name'             : food.food.name,
-                'price'            : food.food.price,
-                'discount'         : food.food.discount,
-                'discounted_price' : food.food.discounted_price,
-                'star_score'       : food.food.star_score,
-                'review_count'     : food.food.review_count,
-                'create_at'        : food.food.create_at
+                'id'               : food.id,
+                'img_url'          : food.foodimage_set.first().image_url,
+                'name'             : food.name,
+                'price'            : food.price,
+                'discount'         : food.discount,
+                'discounted_price' : food.discounted_price,
+                'star_score'       : food.star_score,
+                'review_count'     : food.review_count,
+                'create_at'        : food.create_at
             }
             result.append(food_info)
 
         return JsonResponse({'result' : result}, status=200)
-
-
-# 1. 전체리스트 불러오기 
-# 2. 카테고리 별로 불러오기 (all, 간편식, 밥, 면, 반찬, 간식, 음료)
-# 3. restful.apl방식을 이용해서 필터링하기
-# 4. 불러온 리스트를 별점 높은순, 후기 많은순으로 정렬하기
