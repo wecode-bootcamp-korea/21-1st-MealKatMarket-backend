0fe266a2d15955ed1f5fcb37c9324709303dc376
pkc035 <pkc0305@gmail.com>
2021-06-17 00:14:53 +0900

    Modify : 장바구니 담기 기능 완료

--- carts/views.py
+++ carts/views.py
@@ -1,46 +1,36 @@
-import json
-import jwt
+import json, jwt, requests
 
 from django.http      import JsonResponse
 from django.views     import View
 from django.db.models import Q
 
+from users.utils     import login_decorator
 from .models         import Cart
-from users.models    import User
-from products.models import Food, RequiredOption, SelectOption
-from my_settings     import SECRET_KEY
-
-def decorator_decode():
-    def decode_password(token):
-        decoded_token = jwt.decode(token,SECRET_KEY, algorithms='HS256')
-
-        return decoded_token['id']
 
 class CartView(View):
+    @login_decorator
     def post(self, request):
         data = json.loads(request.body)
 
-        #임시 id 객체 가져오기
-        users = User.objects.all()
+        user = request.users
 
         for food_data in data:
             
             food_count = 0
 
             if Cart.objects.filter(
-                Q(user        = users[2])&
+                Q(user        = user)&
                 Q(option_type = food_data['option_type'])&
                 Q(option_id   = food_data['option_id'])).exists():
 
                 food = Cart.objects.get(
-                    Q(user        = users[2])&
+                    Q(user        = user)&
                     Q(option_type = food_data['option_type'])&
                     Q(option_id   = food_data['option_id']))
                 food_count = food.count
 
             Cart.objects.update_or_create(
-            # user        = decode_password(request.headers['token']),
-                user        = users[2],
+                user        = user,
                 option_type = food_data['option_type'],
                 option_id   = food_data['option_id'],
                 defaults    = {"count" : food_data['count'] + food_count})
